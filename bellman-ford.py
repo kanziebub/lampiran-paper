688769325e28797ff34a903e54ce3bfb2ca2d66f
Nabila Khansa <nabila.khansa@gmail.com>
2021-12-06 14:53:43 +0700

    lampiran paper daa

---
+++ bellman-ford.py
@@ -0,0 +1,147 @@
+# Python3 program for Bellman-Ford's single source
+# shortest path algorithm.
+
+# Class to represent a graph
+class Graph:
+
+ def __init__(self, vertices):
+  self.V = vertices # No. of vertices
+  self.graph = []
+  self.hospitals = []
+
+ # function to add an edge to graph
+ def addEdge(self, u, v, w):
+  self.graph.append([u, v, w])
+  
+ # utility function used to print the solution
+ def printArr(self, dist):
+  print("Vertex Distance from Source")
+  for i in range(self.V):
+   print("{0}\t\t{1}".format(i, dist[i]))
+ 
+ # The main function that finds shortest distances from src to
+ # all other vertices using Bellman-Ford algorithm. The function
+ # also detects negative weight cycle
+ def BellmanFord(self, src):
+
+  # Step 1: Initialize distances from src to all other vertices
+  # as INFINITE
+  dist = [float("Inf")] * self.V
+  dist[src] = 0
+
+
+  # Step 2: Relax all edges |V| - 1 times. A simple shortest
+  # path from src to any other vertex can have at-most |V| - 1
+  # edges
+  for _ in range(self.V - 1):
+   # Update dist value and parent index of the adjacent vertices of
+   # the picked vertex. Consider only those vertices which are still in
+   # queue
+   for u, v, w in self.graph:
+    new = dist[u] + w
+    if dist[u] != float("Inf") and new < dist[v]:
+      dist[v] = new
+
+  # Step 3: check for negative-weight cycles. The above step
+  # guarantees shortest distances if graph doesn't contain
+  # negative weight cycle. If we get a shorter path, then there
+  # is a cycle.
+
+  for u, v, w in self.graph:
+    if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+      print("Graph contains negative weight cycle")
+      return
+      
+  # print all distance
+  self.printArr(dist)
+
+g = Graph(32)
+g.addEdge(0, 1, 65)
+g.addEdge(1, 2, 400)
+g.addEdge(2, 3, 60)
+g.addEdge(3, 4, 750)
+g.addEdge(4, 5, 550)
+g.addEdge(5, 6, 300)
+g.addEdge(6, 7, 650)
+
+g.addEdge(7, 8, 170)
+g.addEdge(8, 9, 180)
+g.addEdge(8, 10, 350)
+g.addEdge(10, 11, 600)
+g.addEdge(11, 12, 1100)
+g.addEdge(12, 13, 300)
+g.addEdge(13, 14, 250)
+
+g.addEdge(13, 15, 290)
+g.addEdge(15, 16, 280)
+g.addEdge(16, 17, 600)
+g.addEdge(17, 18, 270)
+g.addEdge(18, 6, 550)
+g.addEdge(17, 19, 1400)
+g.addEdge(19, 20, 100)
+
+g.addEdge(19, 21, 220)
+g.addEdge(21, 22, 170)
+g.addEdge(22, 1, 650)
+g.addEdge(16, 23, 72)
+g.addEdge(23, 24, 250)
+g.addEdge(23, 31, 1700)
+g.addEdge(24, 25,3300)
+
+g.addEdge(25, 30, 190)
+g.addEdge(30, 31, 140)
+g.addEdge(25, 26, 250)
+g.addEdge(29, 31, 260)
+g.addEdge(28, 29, 140)
+g.addEdge(27, 28, 150)
+g.addEdge(26, 28,27)
+# ==================== reverse
+g.addEdge(1, 0, 65)
+g.addEdge(2, 1, 400)
+g.addEdge(3, 2, 60)
+g.addEdge(4, 3, 750)
+g.addEdge(5, 4, 550)
+g.addEdge(6, 5, 300)
+g.addEdge(7, 6, 650)
+
+g.addEdge(8, 7, 170)
+g.addEdge(9, 8, 180)
+g.addEdge(10, 8, 350)
+g.addEdge(11, 10, 600)
+g.addEdge(12, 11, 1100)
+g.addEdge(13, 12, 300)
+g.addEdge(14, 13, 250)
+
+g.addEdge(15, 13, 290)
+g.addEdge(16, 15, 280)
+g.addEdge(17, 16, 600)
+g.addEdge(18, 17, 270)
+g.addEdge(6, 18, 550)
+g.addEdge(19, 17, 1400)
+g.addEdge(20, 19, 100)
+
+g.addEdge(21, 19, 220)
+g.addEdge(22, 21, 170)
+g.addEdge(1, 22, 650)
+g.addEdge(23, 16, 72)
+g.addEdge(24, 23, 250)
+g.addEdge(31, 23, 1700)
+g.addEdge(25, 24,3300)
+
+g.addEdge(30, 25, 190)
+g.addEdge(31, 30, 140)
+g.addEdge(26, 25, 250)
+g.addEdge(31, 29, 260)
+g.addEdge(29, 28, 140)
+g.addEdge(28, 27, 150)
+g.addEdge(28, 26, 27)
+
+g.addEdge(11, 31, 1200)
+
+
+# Print the solution
+g.BellmanFord(20)
+
+# Initially, Contributed by Neelam Yadav
+# Later On, Edited by Himanshu Garg
+# https://www.geeksforgeeks.org/bellman-ford-algorithm-dp-23/
---
+++ pathfinding.java
@@ -0,0 +1,230 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Math.max;
+
+public class pathfinding {
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+
+        int N = in.nextInt();
+        Graph BSD = new Graph(N);
+
+        String node; String hInput;
+        for (int n = 0; n < N; n++) {
+            node = in.next();
+            hInput = in.next();
+
+            if (hInput.equals("hospital")) BSD.addNode(node, true);
+            else BSD.addNode(node, false);
+        }
+
+        int E = in.nextInt();
+
+        String N1; String N2; int w;
+        for (int e = 0; e < E; e++) {
+            N1 = in.next();
+            N2 = in.next();
+            w = in.nextInt();
+
+            BSD.addEdge(N1, N2, w);
+        }
+
+        String x = "21";
+        String dijkstra = BSD.dijkstraNearestHospital(x);
+
+        out.println(dijkstra);
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+    }
+}
+
+class Graph {
+    int vertices;
+    HashMap<String, Node> nodes;
+    HashMap<String[], Edge> edges;
+
+    // for dijkstra implementation
+    PriorityQueue<Path> pq; 
+    PriorityQueue<Node> hospitals;
+
+    Graph(int vertices) {
+        this.vertices = vertices;
+        nodes = new HashMap<>();
+        edges = new HashMap<>();
+    }
+
+    public void addNode(String name, Boolean isHospital) {
+        Node node = new Node(name, isHospital);
+        nodes.put(name, node);
+    }
+
+    public void addEdge(String n1, String n2, int w) {
+        Node N1; Node N2; 
+        String[] fromto = new String[2];
+        if (nodes.containsKey(n1) && nodes.containsKey(n2)) {
+            N1 = nodes.get(n1); N2 = nodes.get(n2);
+
+            Edge edge1 = new Edge(N1, N2, w);
+            fromto[0] = n1; fromto[1] = n2;
+            edges.put(fromto, edge1);
+            N1.addEdge(edge1);
+
+            Edge edge2 = new Edge(N2, N1, w);
+            fromto[0] = n2; fromto[1] = n1;
+            edges.put(fromto, edge2);
+            N2.addEdge(edge2);
+        }
+    }
+
+    private void clearAll() {
+        for (String N : nodes.keySet()) {
+            nodes.get(N).reset();
+        }
+    }
+
+    private void dijkstra(Node src) {
+        pq = new PriorityQueue<Path>();
+        hospitals = new PriorityQueue<Node>();
+        clearAll();
+
+        pq.add(new Path(src, 0));
+        src.cost = 0;
+
+        int c;
+        while (!(pq.isEmpty())) {
+            Path p = pq.remove();
+            Node n1 = p.dest;
+
+            for (Edge e : n1.edges.keySet()) {
+                Node n2 = e.des;
+                int w = e.weight;
+
+                c = n1.cost + w;
+
+                if (n2.cost > c) {
+                    n2.cost = c;
+                    n2.prev = n1;
+
+                    pq.add(new Path(n2, n2.cost));
+                }
+            }
+        }
+    }
+
+    public String dijkstraNearestHospital(String N) {
+        Node node; int cost = 0; String res = "Dijkstra: ";
+        if (nodes.containsKey(N)) {
+            node = nodes.get(N);
+
+            dijkstra(node);
+
+            for (String n : nodes.keySet()) {
+                node = nodes.get(n);
+                if (node.isHospital) hospitals.add(node);
+            }
+
+            node = hospitals.remove();
+            cost = node.cost;
+            res = res + node.name;
+        }
+
+        
+        res = res + " ";
+        res = res + cost;
+
+        return res;
+    }
+
+}
+
+class Path implements Comparable<Path> {
+    public Node dest;  // w
+    public int cost;   // d(w)
+
+    public Path(Node des, int c) {
+        this.dest = des;
+        this.cost = c;
+    }
+
+    public int compareTo(Path p) {
+        int other = p.cost;
+        return this.cost < other ? -1 : this.cost > other ? 1 : 0;
+    }
+}
+
+class Node implements Comparable<Node> {
+    public String name;
+    public Boolean isHospital;
+    public HashMap<Edge, Integer> edges;
+    // dijkstra
+    public Node prev;
+    public int cost;
+    public HashMap<Path, Integer> paths;
+
+    public Node(String name, Boolean isHospital) {
+        this.name = name;
+        this.isHospital = isHospital;
+        edges = new HashMap<>();
+
+        this.prev = null;
+        this.cost = Integer.MAX_VALUE;
+    }
+
+    public void reset() {
+        this.prev = null;
+        this.cost = Integer.MAX_VALUE;
+    }
+
+    public void addEdge(Edge edge) {
+        edges.put(edge, edge.weight);
+    }
+
+    public int compareTo(Node n) {
+        int other = n.cost;
+        return this.cost < other ? -1 : this.cost > other ? 1 : 0;
+    }
+
+}
+
+class Edge {
+    public Node src;
+    public Node des;
+    public int weight;
+
+    public Edge(Node src, Node des, int weight) {
+        this.src = src;
+        this.des = des;
+        this.weight = weight;
+    }
+}
